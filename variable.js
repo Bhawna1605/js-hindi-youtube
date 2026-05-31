259d24dc97efc897a35a6644b0bc9b1ce80173cd
Bhawna <bhawnajune2006@gmail.com>
2026-06-01 03:04:41 +0530

    Initialize account variables and log their values

---
+++ variable.js
@@ -0,0 +1,11 @@
+const accountId=14453
+let accountEmail= "bhawnajune2006@gmail.com"
+var accountPassword= "12345"
+accountCity= "Haryana"
+//accountId=2 not allowed
+accountEmail= "bs@bs.com"
+accountPassword="21212121"
+accountCity="bengluru"
+//console.log(accountId);
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+//Prefer not to use var because of issue in block scope and functional scope
