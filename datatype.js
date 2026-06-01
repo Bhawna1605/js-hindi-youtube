c682f237d69288ad73c5b8747e96916a6b7cff1d
Bhawna <bhawnajune2006@gmail.com>
2026-06-01 20:46:15 +0000

    Datatypes and ECMA standards

---
+++ datatype.js
@@ -0,0 +1,17 @@
+"use strict";//treat all JS codes as a newer version
+//alert(3+3)//we are using nodejs,not browser
+//console.log(3+3); console.log("Bhawna")//Code readability should be high
+let name="Bhawna"
+let age=20
+let isLoggedIn=false
+let state;
+// number=>2 to power 53
+// BigInt
+// string=>""
+// Boolean=>true/false
+// null=>standalone value 
+// undefined=>
+// symbol=>unique 
+// object
+console.log(typeof null)
+console.log(typeof undefined)
