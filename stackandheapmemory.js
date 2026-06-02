74cc6a87e6e7b1ed5251306aa451df2679a8d1fe
Bhawna <bhawnajune2006@gmail.com>
2026-06-02 14:24:02 +0000

    Concept of stack and heap memory

---
+++ stackandheapmemory.js
@@ -0,0 +1,14 @@
+//Stack(Primitive) and Heap(Non-Primitive)
+let myYouTubeName="bhawnasingladotcom"
+let anotherName=myYouTubeName
+anotherName="chaiaurcode"
+console.log(myYouTubeName);
+console.log(anotherName);
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl",
+}
+let userTwo=userOne 
+userTwo.email="bhawna@google.com"
+console.log(userOne.email)
+console.log(userTwo.email)
