a782c7e4a523345df7ce52135f814f5257a30725
Bhawna <bhawnajune2006@gmail.com>
2026-06-04 16:35:12 +0000

    All about json and api start

---
+++ objectsandapi.js
@@ -0,0 +1,16 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+const {courseInstructor: instructor} = course
+console.log(instructor);
+// const navbar=({company})=>{
+
+// }
+// navbar(company="Bhawna")->de structure for react not js
+[
+    {},
+    {},
+    {}
+]
