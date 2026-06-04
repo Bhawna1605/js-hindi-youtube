fb77df74f5cc32fa7e6559c1983fd4e3ad0b9791
Bhawna <bhawnajune2006@gmail.com>
2026-06-04 17:47:32 +0000

    functions and parameters

---
+++ functions.js
@@ -0,0 +1,34 @@
+// function sayMyName(){
+//     console.log("B");
+//     console.log("H");
+//     console.log("A");
+//     console.log("W");
+//     console.log("N");
+//     console.log("A");
+// }
+// sayMyName()
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+// addTwoNumbers(4,8);
+function addTwoNumbers(number1,number2){
+    // let result=number1+number2
+    // return result
+    return number1+number2
+}
+const result=addTwoNumbers(4,8)
+console.log("Result:",result);
+// function loginUserMessage(username){
+//     return `${username} just logged in`
+// }
+// console.log(loginUserMessage("Bhawna"));
+// console.log(loginUserMessage());
+function loginUserMessage(username){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+//console.log(loginUserMessage("Bhawna"));
+console.log(loginUserMessage());
