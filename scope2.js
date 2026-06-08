e5f20a0826bb47679ef1fc16dfa4054ac439acd1
Bhawna <bhawnajune2006@gmail.com>
2026-06-08 20:07:36 +0000

    function depth scope

---
+++ scope2.js
@@ -0,0 +1,25 @@
+function one(){
+    const username ="Bhawna"
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+//one()
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website="telegram"
+        console.log(username+website);
+    }
+}
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+const addtwo=function(num){
+    return num+2
+}
+addtwo(5)
